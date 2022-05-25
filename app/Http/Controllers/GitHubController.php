5e597f66f830ed75ad4e16c40db1300132161917
Chun Shui <chunshui.tan@capbay.com>
2022-05-25 14:51:32 +0800

    add github login

---
+++ app/Http/Controllers/GitHubController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Exception;
+use Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class GitHubController extends Controller
+{
+    public function gitRedirect()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function gitCallback()
+    {
+        try {
+
+            $user = Socialite::driver('github')->user();
+            $searchUser = User::where('github_id', $user->id)->first();
+
+            if ($searchUser) {
+
+                Auth::login($searchUser);
+
+                return redirect('/');
+            } else {
+                $gitUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'github_id' => $user->id,
+                    'auth_type' => 'github',
+                    'password' => encrypt('gitpwd059')
+                ]);
+
+                Auth::login($gitUser);
+
+                return redirect('/');
+            }
+        } catch (Exception $e) {
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GitHubController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('auth/github', [GitHubController::class, 'gitRedirect']);
+Route::get('auth/github/callback', [GitHubController::class, 'gitCallback']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
